53cf3a97a42c284726108200ea4dc8b6723e4240
Nelson Neto <nelson.ash@outlook.com>
2024-05-24 21:37:24 +0000

    add services

--- src/Plate-Spa/src/app/app.component.ts
+++ src/Plate-Spa/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookService } from './_services/book.service';
+import { CategoryService } from './_services/category.service';
+import { ConfirmationDialogService } from './_services/confirmation-dialog.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookService, CategoryService, ConfirmationDialogService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
